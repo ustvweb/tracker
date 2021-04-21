84d8374e266ae0f3f40d09a21ff26a545bdf8dca
ustvweb <68726308+ustvweb@users.noreply.github.com>
2021-04-21 10:19:25 +0800

    Update message.blade.php

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -6,7 +6,10 @@
             <h1>{{ $message }}</h1>
             <br>
             <p>
-                <a class="btn btn-lg btn-primary" href="/" role="button">@lang('tracker::tracker.go_home')</a>
+                <!--<a class="btn btn-lg btn-primary" href="/" role="button">@lang('tracker::tracker.go_home')</a>-->
+            </p>
+            <p>
+                <a class="btn btn-lg btn-primary" href="{{url('login')}}" role="button">@lang('tracker::tracker.login')</a>
             </p>
         </div>
     </div>
